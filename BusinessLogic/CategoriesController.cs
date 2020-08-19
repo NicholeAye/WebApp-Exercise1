12a1488b5e4dd4fd5991be14c81faa9ea0f570e3
Thidar Aye <thidaraye.zin@gmail.com>
2020-08-20 01:31:09 +0800

    Add files via upload

---
+++ BusinessLogic/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Entities;
+
+
+namespace BusinessLogic
+{
+    public class CategoriesController
+    {
+        CategoriesDataController dataaccess;
+        public CategoriesController()
+        {
+            dataaccess = new CategoriesDataController();
+        }
+
+        public int Insert(xsdCodeTest.CategoriesRow row)
+        {
+            return dataaccess.Insert(row);
+        }
+        public int Update(xsdCodeTest.CategoriesRow row)
+        {
+            return dataaccess.Update(row);
+        }
+
+        public int Delete(xsdCodeTest.CategoriesRow row)
+        {
+            return dataaccess.Delete(row);
+        }
+    }
+
+}
+
